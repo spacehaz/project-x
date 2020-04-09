4a53eda6a5bc6c0e30a37bfe1f6450fb317b709a
spacehaz <spacehaz@gmail.com>
2020-04-09 17:22:56 +0300

    added fix

--- src/helpers/format-item/index.js
+++ src/helpers/format-item/index.js
@@ -15,13 +15,13 @@ export default ({
   return {
     id: itemId[0],
     title: title[0],
-    category: primaryCategory[0].categoryName[0],
+    category: (primaryCategory[0] || {}).categoryName[0],
     image: process.env.NODE_ENV === 'development' ? (galleryURL[0] || '').replace('https://','http://') : (galleryURL[0] || ''),
     url: viewItemURL[0],
-    sellerRanking: 100 - sellerInfo[0].positiveFeedbackPercent[0],
-    paymentMethod: paymentMethod[0],
+    sellerRanking: sellerInfo[0].positiveFeedbackPercent[0],
+    paymentMethod: (paymentMethod || [])[0],
     location: location[0],
     returnsAccepted: Boolean(returnsAccepted[0]),
-    price: sellingStatus[0].convertedCurrentPrice[0]
+    price: (sellingStatus[0]).convertedCurrentPrice[0]
   }
 }
