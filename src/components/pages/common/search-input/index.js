4d66dfc449cf76ac8645507467bcc97324d812aa
spacehaz <spacehaz@gmail.com>
2020-04-15 21:49:13 +0300

    added fix

--- src/components/pages/common/search-input/index.js
+++ src/components/pages/common/search-input/index.js
@@ -47,11 +47,11 @@ export default ({ loading, question, onChange }) => {
         {(question || {}).title}
       </span>
     </div>
-    {renderOptions({ question, changeValue, inputRef, changeId })}
+    {renderOptions({ question, changeValue, inputRef, changeId, value })}
   </div>
 }
 
-const renderOptions = ({ question, changeValue, inputRef, changeId }) => {
+const renderOptions = ({ question, changeValue, inputRef, changeId, value }) => {
   if (!question) { return null }
   if (question.no_options) { return null }
   return <div className={styles.answers}>
@@ -60,7 +60,7 @@ const renderOptions = ({ question, changeValue, inputRef, changeId }) => {
       onClick={_ => {
         console.log('here')
         changeId && changeId(answer.id)
-        changeValue && changeValue(answer.input_title)
+        changeValue && changeValue(`${value} ${answer.input_title}`)
       }}
     >
       {answer.title}
--- src/components/pages/main/header/index.js
+++ src/components/pages/main/header/index.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import styles from './styles.module.scss'
+import { getImages } from 'helpers'
+const images = getImages({ src: 'pattern' })
 
-export default ({ title, description }) => <div className={styles.header}>
-  <h1 className={styles.title}>{title}</h1>
-  <h4 className={styles.description}>{description}</h4>
+export default ({ title, description }) => <div className={styles.header} style={{ backgroundImage: `url(${isHighDensity() || isRetina() ? images.imageRetina : imageRetina.image})` }}>
+  <div className={styles.headerContent}>
+    <h1 className={styles.title}>{title}</h1>
+    <h4 className={styles.description}>{description}</h4>
+  </div>
 </div>
+
+const isHighDensity = () => {
+  return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 124dpi), only screen and (min-resolution: 1.3dppx), only screen and (min-resolution: 48.8dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 1.3), only screen and (-o-min-device-pixel-ratio: 2.6/2), only screen and (min--moz-device-pixel-ratio: 1.3), only screen and (min-device-pixel-ratio: 1.3)').matches)) || (window.devicePixelRatio && window.devicePixelRatio > 1.3))
+}
+
+const isRetina = () => {
+  return ((window.matchMedia && (window.matchMedia('only screen and (min-resolution: 192dpi), only screen and (min-resolution: 2dppx), only screen and (min-resolution: 75.6dpcm)').matches || window.matchMedia('only screen and (-webkit-min-device-pixel-ratio: 2), only screen and (-o-min-device-pixel-ratio: 2/1), only screen and (min--moz-device-pixel-ratio: 2), only screen and (min-device-pixel-ratio: 2)').matches)) || (window.devicePixelRatio && window.devicePixelRatio >= 2)) && /(iPad|iPhone|iPod)/g.test(navigator.userAgent)
+}
