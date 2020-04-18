2cacde8fdc30a2a42754d03dcdc133faf06f66d2
spacehaz <spacehaz@gmail.com>
2020-04-19 01:40:54 +0300

    added fix

--- src/data/api/items/get-items.js
+++ src/data/api/items/get-items.js
@@ -3,5 +3,5 @@ import fetch from '../fetch'
 import { prepareGetParams } from '../helpers'
 
 export default ({ answers, keywords }) => {
-  return fetch(`${API_URL}/items${prepareGetParams({ answers, limit: 100, keywords })}`)
+  return fetch(`${API_URL}/items${prepareGetParams({ answers, limit: 500, keywords })}`)
 }
