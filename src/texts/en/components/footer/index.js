f4c1285f592659630b26cb875a94e143b4befd20
spacehaz <spacehaz@gmail.com>
2020-05-03 02:30:37 +0300

    added fix

--- src/texts/en/components/footer/index.js
+++ src/texts/en/components/footer/index.js
@@ -1,5 +1,6 @@
 export default {
-  copyright: 'Covid Sex Partner',
-  madeBy: 'Made by Grok - AI shopping assistant',
-  contact: 'Contact {{email}}' 
+  copyright: 'Made by Grok - AI shopping assistant',
+  testMode: 'We are working in test mode!',
+  contact: 'Please share your opinion via mail to <a href="mailto:hi@sexisolation.com">hi@sexisolation.com</a>' 
 }
+
